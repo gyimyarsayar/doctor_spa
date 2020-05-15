aeb6d22de0f8402bdcd195fd8323319557686106
Htet Lin Maung <jsthtet96@gmail.com>
2020-05-15 21:59:55 +0630

    add overview card

--- src/components/home/OverviewCard.jsx
+++ src/components/home/OverviewCard.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   card: {
-    background: "#FFFFFF 0% 0% no-repeat padding-box",
+    background: "#FFFFFF",
     boxShadow: "0px 2px 6px #0000000A",
     borderRadius: 4,
-    width: 214,
-    height: 85
+    height: 85,
+    padding: "0 1.1rem",
+    boxSizing: "border-box",
+    minWidth: 214
+  },
+  label: {
+    fontSize: 15,
+    color: "#8288AC",
+    letterSpacing: 0
+  },
+  grid: {
+    flexGrow: 1,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-between"
   }
 }));
 
-const OverviewCard = () => {
+const OverviewCard = ({ name, count, img }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.card}>
-      <CardContent></CardContent>
+      <Grid container className={classes.grid}>
+        <Grid direction="column">
+          <Typography className={classes.label}>{count}</Typography>
+          <Typography className={classes.label}>{name}</Typography>
+        </Grid>
+        <Grid>
+          <img src={img} alt="" />
+        </Grid>
+      </Grid>
     </Card>
   );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,6 +5,16 @@ import Typography from "@material-ui/core/Typography";
 
 import OverviewCard from "../components/home/OverviewCard";
 
+import doctor from "../static/images/Group 2765.png";
+import nurse from "../static/images/Group 2660.png";
+import heart from "../static/images/Group 2659.png";
+import pharmastist from "../static/images/Group 2655.png";
+import Laboratorst from "../static/images/Intersection 7.png";
+import accountants from "../static/images/Intersection 6.png";
+import operation from "../static/images/Intersection 9.png";
+import birth from "../static/images/Symbol 2 – 1.png";
+import death from "../static/images/Group 2664.png";
+
 const useStyles = makeStyles((theme) => ({
   title: {
     fontSize: 20,
@@ -15,6 +25,54 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
   const classes = useStyles();
 
+  const overviews = [
+    {
+      name: "Doctors",
+      count: 20,
+      img: doctor
+    },
+    {
+      name: "Nurses",
+      count: 25,
+      img: nurse
+    },
+    {
+      name: "Patient",
+      count: 50,
+      img: heart
+    },
+    {
+      name: "Pharmastist",
+      count: 10,
+      img: pharmastist
+    },
+    {
+      name: "Laboratorst",
+      count: 4,
+      img: Laboratorst
+    },
+    {
+      name: "Accountants",
+      count: 4,
+      img: accountants
+    },
+    {
+      name: "Operation",
+      count: 50,
+      img: operation
+    },
+    {
+      name: "Birth Report",
+      count: 2,
+      img: birth
+    },
+    {
+      name: "Death Reports",
+      count: 0,
+      img: death
+    }
+  ];
+
   return (
     <Grid container spacing={3} style={{ margin: 0 }}>
       <Grid item xs={8}>
@@ -23,9 +81,24 @@ const Home = () => {
       <Grid item xs={4}>
         <Typography className={classes.title}>Budget Report</Typography>
       </Grid>
-      <Grid item>
-        <OverviewCard />
+      <Grid
+        container
+        item
+        xs={8}
+        spacing={3}
+        style={{ justifyContent: "space-between" }}
+      >
+        {overviews.map((overview, i) => (
+          <Grid item key={i}>
+            <OverviewCard
+              name={overview.name}
+              count={overview.count}
+              img={overview.img}
+            />
+          </Grid>
+        ))}
       </Grid>
+      ,<Grid item xs={4}></Grid>
     </Grid>
   );
 };
