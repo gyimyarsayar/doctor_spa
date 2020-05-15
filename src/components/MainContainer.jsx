b516cea9382e2e32b463bfe33734f1e46a72cc6c
Htet Lin Maung <jsthtet96@gmail.com>
2020-05-15 17:20:18 +0630

    add source san pro fonts

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { connect } from "react-redux";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { kDrawerWidth } from "../constants";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("padding", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    paddingLeft: 73
+  },
+  contentShift: {
+    transition: theme.transitions.create("padding", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    paddingLeft: kDrawerWidth
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar
+  }
+}));
+
+const MainContainer = ({ children, open }) => {
+  const classes = useStyles();
+
+  return (
+    <main
+      className={clsx(classes.content, {
+        [classes.contentShift]: open
+      })}
+    >
+      <div className={classes.toolbar} />
+      {children}
+    </main>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  open: state.main.drawerOpen,
+  children: ownProps.children
+});
+
+export default connect(mapStateToProps)(MainContainer);
